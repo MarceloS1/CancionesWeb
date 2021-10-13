e80ca7acbe9868bd2e7919f7e005da992258c3f8
Mache <mache.sanchezt@gmail.com>
2021-10-13 10:12:38 -0400

    AppDbContext Creado

---
+++ CancionesWeb/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CancionesWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CancionesWeb.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Canciones> Cancion { get; set; }
+    }
+}
